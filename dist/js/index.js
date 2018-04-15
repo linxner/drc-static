d2038ba5d0467b9220161d70112e9e673ed09141
lingyun-zhang <342709615@qq.com>
2018-04-15 22:27:30 +0800

    IE兼容问题

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,25 @@
-$(function(){window.onscroll=function(){$(".navbox").addClass("view"),$(window).scrollTop()<=1&&$(".navbox").removeClass("view")},document.getElementById("drc-video").playbackRate=1,$(".closebtn").on("click",function(){document.getElementById("drc-video").load()});var n=$(".navbox").height();$(".menu-list").on("click","li",function(){var o=$(this).data("href"),e=document.querySelector(o);$("html,body").stop().animate({scrollTop:e.offsetTop-n},600)}),$(".fa-wechat").hover(function(){$(".weixin").show()},function(){$(".weixin").hide()})});
+$(function () {
+    window.onscroll = function () {
+        window.onscroll = function () {
+            $('.navbox').addClass('view')
+            if ($(window).scrollTop() <= 1) {
+                $('.navbox').removeClass('view')
+            }
+        }
+        // get the window scroll height
+        var nowscrollHeight = $(window).scrollTop();
+        if (nowscrollHeight >= 10) {
+            $('.navbox').addClass('view')
+        } else {
+            $('.navbox').removeClass('view')
+        }
+    }
+        document.getElementById("drc-video").playbackRate = 1, $(".closebtn").on("click", function () {
+        document.getElementById("drc-video").load()
+    });
+    var n = $(".navbox").height();
+    $(".menu-list").on("click", "li", function () {
+        var o = $(this).data("href"), e = document.querySelector(o);
+        $("html,body").stop().animate({scrollTop: e.offsetTop - n}, 600)
+    })
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,21 @@
 /**
  * Created by zly on 2018/3/19.
  */
-$(function() {
-  window.onscroll = function() {
-    $(".navbox").addClass("view"),
-      $(window).scrollTop() <= 1 && $(".navbox").removeClass("view");
-  };
-  (document.getElementById("drc-video").playbackRate = 1),
-    $(".closebtn").on("click", function() {
-      document.getElementById("drc-video").load();
+$(function () {
+    window.onscroll = function () {
+        $(".navbox").addClass("view"),
+        $(window).scrollTop() <= 1 && $(".navbox").removeClass("view");
+    };
+    (document.getElementById("drc-video").playbackRate = 1),
+        $(".closebtn").on("click", function () {
+            document.getElementById("drc-video").load();
+        });
+    var navheight = $(".navbox").height();
+    $(".menu-list").on("click", "li", function () {
+        var liname = $(this).data("href");
+        var target = document.querySelector(liname);
+        $("html,body")
+            .stop()
+            .animate({scrollTop: target.offsetTop - navheight}, 600);
     });
-  var navheight = $(".navbox").height();
-  $(".menu-list").on("click", "li", function() {
-    var liname = $(this).data("href");
-    var target = document.querySelector(liname);
-    $("html,body")
-      .stop()
-      .animate({ scrollTop: target.offsetTop - navheight }, 600);
-  });
-  $(".fa-wechat").hover(
-    function() {
-      $(".weixin").show();
-    },
-    function() {
-      $(".weixin").hide();
-    }
-  );
 });
